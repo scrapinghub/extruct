fb64f6b7f783cafb9828798109fb7031459b7c39
Paul Tremberth <paul.tremberth@gmail.com>
2014-10-15 23:37:16 +0200

    Add support for un-nested items (with "iid" references)

--- schemaorg/w3cmicrodata.py
+++ schemaorg/w3cmicrodata.py
@@ -24,9 +24,12 @@ class MicrodataExtractor(object):
     _xp_item_docid = lxml.etree.XPath("""count(preceding::*[@itemscope])
                                         + count(ancestor::*[@itemscope])
                                         + 1""")
-    _xp_norm = lxml.etree.XPath('normalize-space(.)')
 
+    def __init__(self, nested=True):
+        self.nested = nested
 
+    def get_docid(self, node):
+        return int(self._xp_item_docid(node))
 
     def extract(self, htmlstring, url='http://www.example.com/', encoding="UTF-8"):
         self.url = url
@@ -40,21 +43,42 @@ class MicrodataExtractor(object):
                                         for item in self._xp_item(document)])}
 
     def extract_item(self, node):
-        itemid = self._xp_item_docid(node)
-        if itemid in self.items_seen:
-            return
-        self.items_seen.add(itemid)
+        itemid = self.get_docid(node)
+
+        if self.nested:
+            if itemid in self.items_seen:
+                return
+            self.items_seen.add(itemid)
 
         item = {}
+        if not self.nested:
+            item["iid"] = itemid
         types = node.get('itemtype', '').split()
         if types:
             item["type"] = types
+
+            itemid = node.get('itemid')
+            if itemid:
+                item["id"] = itemid.strip()
 
         properties = collections.defaultdict(list)
         for name, value in self.extract_properties(node):
             properties[name].append(value)
 
-        item["properties"] = dict(properties.items())
+        props = []
+        for (name, values) in properties.items():
+            if len(values) == 1:
+                props.append((name, values[0]))
+            else:
+                props.append((name, values))
+
+        item["properties"] = dict(props)
+
+        # not in the specs, but can be handy
+        #textContent = self.extract_textContent(node)
+        #if textContent:
+            #item["textContent"] = textContent
+
         return item
 
     def extract_properties(self, node):
@@ -70,7 +94,10 @@ class MicrodataExtractor(object):
     def extract_property_value(self, node):
         #http://www.w3.org/TR/microdata/#values
         if node.get("itemscope") is not None:
-            return self.extract_item(node)
+            if self.nested:
+                return self.extract_item(node)
+            else:
+                return {"iid_ref": self.get_docid(node)}
 
         elif node.tag == "meta":
             return node.get("content", "")
@@ -90,9 +117,13 @@ class MicrodataExtractor(object):
         elif node.tag in ("time",):
             return node.get("datetime", "")
 
-        elif node.get("content"):
-            return node.get("content", "")
+        # not in W3C specs but used in schema.org examples
+        elif node.tag in ("meta",) and node.get("content"):
+            return node.get("content")
 
         else:
-            return lxml.html.tostring(node, method="text", encoding=unicode,
-                        with_tail=False).strip()
+            return self.extract_textContent(node)
+
+    def extract_textContent(self, node):
+        return lxml.html.tostring(node, method="text", encoding=unicode,
+                    with_tail=False).strip()

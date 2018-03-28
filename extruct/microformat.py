f12733b1709cf85f47d9dea6b85c330533a9db00
Ludovica Gonella <ludovica.gonella@gmail.com>
2018-03-28 11:12:57 -0700

    remove flattening of h-entry

--- extruct/microformat.py
+++ extruct/microformat.py
@@ -7,8 +7,4 @@ class MicroformatExtractor(object):
 
     def extract_items(self, html, url, document=None):
         for obj in mf2py.parse(html, html_parser="lxml", url=url)['items']:
-            if obj['type'] == 'h-entry':
-                for o in obj['children']:
-                    yield o
-            else:
-                yield obj
+            yield obj

59e611eb662d5065e1375041d3e84a2b61142a46
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2018-03-30 17:04:33 +0100

    Added a JSON hot-swapping utility in utils.py, plus tests.

---
+++ tests/test_utils.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import unittest
+import json
+
+from extruct import utils
+
+
+# Python 2 is a vengeful fossil
+native_json_exc = getattr(json, 'JSONDecodeError', ValueError)
+
+
+class NotAJSONDecodeError(ValueError):
+    pass
+
+
+class _json_shimmer:
+    def __init__(self):
+        self.invoked = False
+
+    def decode(self, json_string):
+        if json_string == 'FAIL WITH VALUEERROR':
+            raise ValueError("Yes sir")
+        try:
+            return json.loads(json_string)
+        except native_json_exc:
+            raise NotAJSONDecodeError("This operation totally failed")
+        finally:
+            self.invoked = True
+
+
+class TestJson(unittest.TestCase):
+
+    def test_json_abstraction(self):
+        # Use default decoder
+        self.assertEqual(utils._json_decoder, json.loads)
+        self.assertEqual(utils._json_decoder_raises, tuple())
+        self.assertEqual(utils.json_loads('{}'), {})
+        with self.assertRaises(native_json_exc):  # ugh, Python 2
+            utils.json_loads('{')
+        # Set decoder, try again
+        shimmer = _json_shimmer()
+        utils.set_json_decoder(shimmer.decode, (NotAJSONDecodeError,))
+        self.assertEqual(utils._json_decoder, shimmer.decode)
+        self.assertEqual(utils._json_decoder_raises, (NotAJSONDecodeError,))
+        self.assertEqual(utils.json_loads('{}'), {})
+        # ensure utils.json_loads didn't call a stale reference to json.loads
+        self.assertTrue(shimmer.invoked)
+        # Specified exceptions should be converted to JSONDecodeErrors.
+        with self.assertRaises(native_json_exc):
+            utils.json_loads('{')
+        # Others should not.
+        with self.assertRaises(ValueError):
+            utils.json_loads('FAIL WITH VALUEERROR')

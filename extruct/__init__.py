a10837aef952b47fdb3327b05027952ff32af311
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2018-05-14 15:42:24 +0300

    Import individual extractors as well
    Thanks for the catch @kmike!

--- extruct/__init__.py
+++ extruct/__init__.py
@@ -1,1 +1,7 @@
 from ._extruct import SYNTAXES, extract
+from .jsonld import JsonLdExtractor
+from .rdfa import RDFaExtractor
+from .w3cmicrodata import MicrodataExtractor
+from .opengraph import OpenGraphExtractor
+from .microformat import MicroformatExtractor
+from .xmldom import XmlDomHTMLParser

161529c77a1e8a623c19bf51f9d5b4a7b3d734fd
jakubwasikowski <jakub.wasikowski@gmail.com>
2018-06-01 11:15:50 +0200

    Add test case to check if returning node works properly.

--- tests/test_extruct.py
+++ tests/test_extruct.py
@@ -17,25 +17,50 @@ class TestGeneric(unittest.TestCase):
         self.assertEqual(jsonize_dict(data), expected)
 
     def test_microdata_custom_url(self):
-        body, expected = self._microdata_custom_url()
+        body, expected = self._microdata_custom_url('product_custom_url.json')
         data = extruct.extract(body, base_url='http://some-example.com',
                                syntaxes=['microdata'])
         self.assertEqual(data, expected)
 
+    def test_microdata_with_returning_node(self):
+        body, expected = self._microdata_custom_url('product_custom_url_and_node_id.json')
+        data = extruct.extract(body, base_url='http://some-example.com',
+                               syntaxes=['microdata'], return_html_node=True)
+        self._replace_node_ref_with_node_id(data)
+        self.assertEqual(data, expected)
+
     def test_deprecated_url(self):
-        body, expected = self._microdata_custom_url()
+        body, expected = self._microdata_custom_url('product_custom_url.json')
         data = extruct.extract(body, url='http://some-example.com',
                                syntaxes=['microdata'])
         self.assertEqual(data, expected)
 
     def test_extra_kwargs(self):
-        body, expected = self._microdata_custom_url()
+        body, expected = self._microdata_custom_url('product_custom_url.json')
         with self.assertRaises(TypeError):
             extruct.extract(body, foo='bar')
 
-    def _microdata_custom_url(self):
+    def _microdata_custom_url(self, test_file):
         body = get_testdata('schema.org', 'product.html')
         expected = {'microdata': json.loads(
-            get_testdata('schema.org', 'product_custom_url.json')
+            get_testdata('schema.org', test_file)
             .decode('UTF-8'))}
         return body, expected
+
+    def _replace_node_ref_with_node_id(self, item):
+        if isinstance(item, list):
+            for i in item:
+                self._replace_node_ref_with_node_id(i)
+        if isinstance(item, dict):
+            for key in list(item):
+                val = item[key]
+                if key == "htmlNode":
+                    item["_nodeId_"] = val.get("id")
+                    del item[key]
+                else:
+                    self._replace_node_ref_with_node_id(val)
+
+
+
+
+

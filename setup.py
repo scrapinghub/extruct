9bfd9d36125b0e7f57c676d682f66f5b22a5b0a7
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2020-12-12 13:15:44 +0300

    declare support for python 3.8 and 3.9 since they are now tested

--- setup.py
+++ setup.py
@@ -56,5 +56,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )

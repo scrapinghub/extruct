f2fc2231706905de1b818fae2040ef61ed2c4418
Steve Dignam <steve@dignam.xyz>
2022-11-13 12:42:15 -0500

    fix lint

--- extruct/xmldom.py
+++ extruct/xmldom.py
@@ -3,8 +3,13 @@ from copy import copy, deepcopy
 from xml.dom import Node
 from xml.dom.minidom import Attr, NamedNodeMap
 
-from lxml.etree import (ElementBase, XPath, _ElementStringResult,
-                        _ElementUnicodeResult, tostring)
+from lxml.etree import (
+    ElementBase,
+    XPath,
+    _ElementStringResult,
+    _ElementUnicodeResult,
+    tostring,
+)
 from lxml.html import HtmlElementClassLookup, HTMLParser
 
 

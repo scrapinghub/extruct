e4b0530b19f188984ba5c6219decdbb76d454d5c
Steve Dignam <steve@dignam.xyz>
2022-11-29 21:02:43 -0500

    cut py36 support

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",

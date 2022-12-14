0c1d8d9e06243edcde80238d79e18a717c2cce61
Steve Dignam <steve@dignam.xyz>
2022-12-13 23:05:51 -0500

    fix

--- extruct/rdfa.py
+++ extruct/rdfa.py
@@ -17,7 +17,7 @@ from pyRdfa import Options
 from pyRdfa import pyRdfa as PyRdfa
 from pyRdfa.initialcontext import initial_context
 from rdflib import Graph
-from rdflib import logger as rdflib_logger
+from rdflib import logger as rdflib_logger  # type: ignore[no-redef]
 
 from extruct.utils import parse_xmldom_html
 
